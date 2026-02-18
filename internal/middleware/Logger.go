d271adea7531a72d27f0f459d2c049394c99874b
arinprajapati <arinprajapati78@gmail.com>
2026-02-18 22:37:39 +0530

    feat: log HTTP response status code in the logger middleware.

--- internal/middleware/Logger.go
+++ internal/middleware/Logger.go
@@ -8,13 +8,26 @@ import (
 	"github.com/google/uuid" // id generator
 )
 
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.statusCode = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
 func Logger(fn http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		start := time.Now()
 		id := uuid.New().String()
+		sr := &statusRecorder{
+			ResponseWriter: res,
+			statusCode:     http.StatusOK,
+		}
+		fn.ServeHTTP(sr, req)
 
-		fmt.Printf("%s %v %s %s\n", req.Method, start.Format(time.RFC3339), id, req.URL.Path)
-		fn.ServeHTTP(res, req)
-		fmt.Printf("%s %v %s %s\n", req.Method, time.Since(start), id, req.URL.Path)
+		fmt.Printf("[%d] %s %v %s %s\n", sr.statusCode, req.Method, time.Since(start), id, req.URL.Path)
 	})
 }
